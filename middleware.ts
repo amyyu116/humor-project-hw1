f6264db65b21f2cadd936be20cfba07874f7e0f0
Amy Yu <amyyu116@gmail.com>
2026-02-27 17:51:30 -0500

    assignment 5, middleware hotfix

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
-// c:\Users\amyyu\Downloads\S26 HW\humor-project-hw\middleware.ts
-
 import { type NextRequest } from "next/server";
-import { updateSession } from "./utils/supabase/middleware";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
     return await updateSession(request);
@@ -10,12 +8,12 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
+         * Match all request paths except:
+         * - _next (static files)
+         * - favicon
+         * - images
+         * - Supabase auth
          */
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+        "/((?!_next/static|_next/image|favicon.ico|auth/v1).*)",
     ],
 };
